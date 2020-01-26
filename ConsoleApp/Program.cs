0758d0c66c80ce889e6dabaa6780236158609223
Lofty Mettyer <mettyer@gmail.com>
2020-01-26 07:44:09 +0000

    Fiel system watcher in the console for mock text data files

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,24 +10,52 @@ namespace ConsoleApp
     class Program
     {
 
+        private static string watchPath = AppDomain.CurrentDomain.BaseDirectory + "Mock\\";
+        private static readonly FileSystemWatcher Watcher = new FileSystemWatcher();
+        private static string url = "https://localhost:44344/api/trade";
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("Simple harness of api!");
             Console.WriteLine("Curious known bug if you're using Visual Studio 2017 - multiple startup projects don't seem to work very well the first time you run the project. If this happens, can you just try running the project again");
 
-            string url = "https://localhost:44344/api/trade";
+            // Listen for file changes
+            Console.WriteLine(string.Format("Listening to path {0}", Watcher.Path));
+            Watcher.Path = watchPath;
+            ProcessDirectory(watchPath);
+
+            Console.WriteLine("Press 'q' to quit.");
+
+            // Watch for changes in LastAccess and LastWrite times, and
+            // the renaming of files or directories.
+            //watcher.NotifyFilter = NotifyFilters.LastAccess
+            //                     | NotifyFilters.LastWrite
+            //                     | NotifyFilters.FileName
+            //                     | NotifyFilters.DirectoryName;
+            Watcher.NotifyFilter = NotifyFilters.LastWrite;
+            // Only watch text files.
+            Watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            Watcher.Changed += OnFileChanged;
+        //    watcher.Created += OnChanged;
+
+            // Begin watching.
+            Watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            while (Console.Read() != 'q') ;
 
 
-            //string details = CallRestMethod(url);
-            //Console.WriteLine(details);
-            var trade = MockTradeDataStream.GetTradeStream("MockSingleTradeData");
+            //    //string details = CallRestMethod(url);
+            //    //Console.WriteLine(details);
+            //    var trade = MockTradeDataStream.GetTradeStream("MockSingleTradeData");
 
-            PostStream(url, trade);
+            //PostStream(url, trade);
 
-            PostStream(url, MockTradeDataStream.GetTradeStream("MockMultipleTradeData"));
+            //PostStream(url, MockTradeDataStream.GetTradeStream("MockMultipleTradeData"));
 
-
-            Console.ReadKey();
         }
 
 
@@ -41,14 +69,12 @@ namespace ConsoleApp
             }
         }
 
-
         public static void PostRestDataAsync(string url, string content)
         {
             var result = url
                 .WithHeader("Accept", "application/json")
                 .PostJsonAsync(content);
         }
-
 
         public static string CallRestMethod(string url)
         {
@@ -62,6 +88,51 @@ namespace ConsoleApp
             result = responseStream.ReadToEnd();
             webresponse.Close();
             return result;
+        }
+
+        private static void OnFileChanged(object source, FileSystemEventArgs e)
+        {
+
+            try
+            {
+                Watcher.Changed -= OnFileChanged;
+                Watcher.EnableRaisingEvents = false;
+                ProcessFile(e.Name);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                Watcher.Changed += OnFileChanged;
+                Watcher.EnableRaisingEvents = true;
+            }
+
+            ProcessFile(e.FullPath);
+        }
+
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory, "*.txt");
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+        }
+
+        public static void ProcessFile(string path)
+        {
+            Console.WriteLine($"Processing File: {path}...");
+
+            string jsonData = File.ReadAllText(path);
+            var mockTrade = new MemoryStream(Encoding.UTF8.GetBytes(jsonData ?? ""));
+
+            PostStream(url, mockTrade);
+
+            //      string text = File.ReadAllText(path);
+            //    Console.WriteLine(text);
+
         }
 
     }
--- ConsoleAppNode/app.js
+++ ConsoleAppNode/app.js
@@ -1,7 +1,8 @@
-'use strict';
-
-//var request = require("request");
-const https = require('https');
+const https = require('https')
+const http = require('http')
+var concat = require('concat-stream');
+var request = require('request');
+var watch = require('node-watch');
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -9,130 +10,52 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-rl.question("What identifer would you like to retrive ? ", function (identifier) {
-    console.log(`Retreiving "${identifier}", ...`);
-
-    //https.get(`https://localhost:44344/api/trade/${identifier}`, (resp) => {
-
-    //    console.dir(JSON.parse(resp));
-
-    //    let data = '';
-
-    //    // A chunk of data has been recieved.
-    //    resp.on('data', (chunk) => {
-    //        data += chunk;
-    //    });
-
-    //    // The whole response has been received. Print out the result.
-    //    resp.on('end', () => {
-    //        console.log(JSON.parse(data).explanation);
-    //    });
-
-    //}).on("error", (err) => {
-    //    console.log("Error: " + err.message);
-    //});
-
-
-    request.get(`https://localhost:44344/api/trade/${identifier}`, (error, response, body) => {
-        if (error) {
-            return console.dir(error);
-        }
-        console.dir(JSON.parse(body));
-    });
-
-  //  rl.close();
+watch('./', { recursive: false }, function (evt, name) {
+    console.log('%s changed.', name);
 });
 
-//rl.on("close", function () {
-//    console.log("\nBYE BYE !!!");
-//    process.exit(0);
-//});
 
-//var rp = require('request-promise');
-//var readline = require('readline-sync');
-//const request = require('request');
+//function promptForData() {
+    //rl.question("What identifer would you like to retrive ? ", function (identifier) {
+    //    console.log(`Retreiving "${identifier}", ...`);
 
-//var querystring = require('querystring');
-//var http = require('http');
+    //    var options = {
+    //        hostname: 'localhost',
+    //        port: 44344,
+    //        path: `api/trade/${identifier}`,
+    //        method: 'GET',
+    //        rejectUnauthorized: false
+    //    }
 
-//var rawData = {
-//    "identifier": "MKS",
-//    "orderType": 0,
-//    "orderSize": 123,
-//    "orderSubType": 0,
-//    "instruction": "fill",
-//    "orderTransmissionType": 1
-//};
+    //    var req = https.request(options, res => {
+    //        console.log(`statusCode: ${res.statusCode}`)
 
-//var options = {
-//    method: 'POST',
-//    uri: 'https://localhost:44344/api/trade/',
-//    body: rawData,
-//    json: true // Automatically stringifies the body to JSON
-//};
+    //        res.on('data', d => {
+    //            process.stdout.write(d)
+    //        })
+    //    })
 
-//rp(options)
-//    .then(function (parsedBody) {
-//        console.log(parsedBody);
-//        // POST succeeded...
-//    })
-//    .catch(function (err) {
-//        console.log(parsedBody);
-//        // POST failed...
-//    });
+    //    req.on('error', error => {
+    //        console.error(error)
+    //    })
 
-//request.post('https://localhost:44344/api/trade', {
-//    json: {
-//        todo: 'Buy the milk'
-//    }
-//}, (error, res, body) => {
-//    if (error) {
-//        console.error(error)
-//        return
-//    }
-//    console.log(`statusCode: ${res.statusCode}`)
-//    console.log(body)
-//});
+    //    //req.end(function () {
+    //    //    promptForData();
+    //    //}
 
-
-//PostCode("hello");
-
-//readline.question("Press any key to continue");
-
-
-//function PostCode(codestring) {
-//    // Build the post string from an object
-//    var post_data = querystring.stringify({
-//        'compilation_level': 'ADVANCED_OPTIMIZATIONS',
-//        'output_format': 'json',
-//        'output_info': 'compiled_code',
-//        'warning_level': 'QUIET',
-//        'js_code': codestring
-//    });
-
-//    // An object of options to indicate where to post to
-//    var post_options = {
-//        host: ' https://localhost:44344/api/trade',
-//        port: '44344',
-//        path: '/compile',
-//        method: 'POST',
-//        headers: {
-//            'Content-Type': 'application/x-www-form-urlencoded',
-//            'Content-Length': Buffer.byteLength(post_data)
-//        }
-//    };
-
-//    // Set up the request
-//    var post_req = http.request(post_options, function (res) {
-//        res.setEncoding('utf8');
-//        res.on('data', function (chunk) {
-//            console.log('Response: ' + chunk);
-//        });
-//    });
-
-//    // post the data
-//    post_req.write(post_data);
-//    post_req.end();
-
+    //    req.end();
+    //});
 //}
+
+rl.question("Press any key to exist... ", function (identifier) {
+}
+
+rl.on("close", function () {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+    });
+
+
+
+//    promptForData();
+
